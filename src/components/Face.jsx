84eb78b6b88d606b137d34ea2ead985859ef4326
Dan Schmitt <ttimhcsnad@Dans-MacBook-Pro.local>
2019-05-24 22:01:05 -0400

    use css3d rotations

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Face.css";
+
+const FlipDirection = {
+  LEFT: "left",
+  RIGHT: "right",
+};
 
 const Face = (props) => {
   const { image, flipping, onSideFlip, currentSide, speed } = props;
+  const [ dir, setDir ] = useState((currentSide) ? FlipDirection.RIGHT : FlipDirection.LEFT);
+
+  const handleAnimIteration = () => {
+    setDir(dir === FlipDirection.LEFT ? FlipDirection.RIGHT : FlipDirection.LEFT);
+    onSideFlip();
+  };
 
   return (
     <div className="Face">
@@ -10,13 +21,14 @@ const Face = (props) => {
         src={image}
         alt="coin face"
         width="200"
-        className={`${currentSide ? "active" : ""} ${
-          flipping ? "flipping" : ""
-        }`}
-        onAnimationIteration={onSideFlip}
+        className={`
+          ${currentSide ? "active" : ""} 
+          ${flipping ? `flipping-${dir}` : ""}
+        `}
+        onAnimationIteration={handleAnimIteration}
         style={{
           animationDuration: `${speed}s`,
-          transition: `transform ${speed * 2}s ease-out`,
+          transition: `transform ${speed * 6}s ease-out`,
         }}
       />
     </div>
